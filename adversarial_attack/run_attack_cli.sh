007fe288beb97438efd7db139f87481729cc0304
Vo Hoang Hoa Vien <vienvhh@dgroup.co>
2025-06-03 11:12:10 +0700

    debug run_attack_cli.sh

--- adversarial_attack/run_attack_cli.sh
+++ adversarial_attack/run_attack_cli.sh
@@ -1,3 +1,4 @@
+# --- run_attack.sh ---
 #!/bin/bash
 
 weights_path="/kaggle/input/resnet50-multilabel-model-add-layers/resnet50_multilabel_model_add_layers.pth"
@@ -7,15 +8,18 @@ mkdir -p "$output_dir"
 
 for i in {0..299}
 do
-  image="/kaggle/input/celebamaskhq/CelebAMask-HQ/CelebA-HQ-img/${i}.jpg"
+  original="/kaggle/input/celebamaskhq/CelebAMask-HQ/CelebA-HQ-img/${i}.jpg"
+  input="/kaggle/input/sd2-new-prompt-clip-classifier/edit_results/sample_${i}/res_1.jpg"
   mask="/kaggle/input/heatmap-mask/kaggle/working/mask/results_${i}.png"
   sample_output="${output_dir}/sample_${i}"
 
   echo "Running attack on sample ${i}"
+  mkdir -p "$sample_output"
 
-  python3 run_attack_cli.py \
+  python3 -m run_attack_cli.py \
     --weights "$weights_path" \
-    --image "$image" \
+    --original "$original" \
+    --input "$input" \
     --mask "$mask" \
     --output_dir "$sample_output"
 done
