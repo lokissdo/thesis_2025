e8504ec92ea747440828ea4bbcf75272963ba111
Vo Hoang Hoa Vien <vienvhh@dgroup.co>
2025-06-04 16:45:14 +0700

    fix path for .sh

--- evaluate/ACE/data_transfer_eval.sh
+++ evaluate/ACE/data_transfer_eval.sh
@@ -3,4 +3,4 @@ ROOT_DIR="/kaggle/input/adversarial-attack-image-eatmap-mask-epsilon03/output_im
 OUT_DIR="/kaggle/working"
 EVAL_MODEL_DIR="/kaggle/working/thesis_2025/evaluate"
 
-python3 data_transfer_eval.py --root_dir "$ROOT_DIR" --out_dir "$OUT_DIR" -- eval_model_dir "$EVAL_MODEL_DIR" --gpu 0
+python3 data_transfer_eval.py --root_dir "$ROOT_DIR" --out_dir "$OUT_DIR" --eval_model_dir "$EVAL_MODEL_DIR" --gpu 0
