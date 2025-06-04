c417b5c53923da5f25126c14d1b0ec0d3fb66628
Vo Hoang Hoa Vien <vienvhh@dgroup.co>
2025-06-04 16:35:45 +0700

    fix path for .sh

--- evaluate/ACE/compute_FID.sh
+++ evaluate/ACE/compute_FID.sh
@@ -1,4 +1,4 @@
 pip install pytorch-fid
-REAL_PATH=/kaggle/working/thesis_2025/evaluate/original
-CF_PATH=/kaggle/working/thesis_2025/evaluate/adversarial
+REAL_PATH=/kaggle/working/original
+CF_PATH=/kaggle/working/adversarial
 python -m pytorch_fid ${REAL_PATH} ${CF_PATH}
