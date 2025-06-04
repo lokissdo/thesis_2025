6921fcf2477c561be23e28357541be3d820437ed
Vo Hoang Hoa Vien <vienvhh@dgroup.co>
2025-06-04 16:40:03 +0700

    fix path for .sh

--- evaluate/ACE/data_transfer_eval.sh
+++ evaluate/ACE/data_transfer_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROOT_DIR="/kaggle/input/adversarial-attack-image-saliency-blended/output_images"
+ROOT_DIR="/kaggle/input/adversarial-attack-image-eatmap-mask-epsilon03/output_images"
 OUT_DIR="/kaggle/working"
 
 python3 data_transfer_eval.py --root_dir "$ROOT_DIR" --out_dir "$OUT_DIR"
